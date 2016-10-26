b6f0c007436845c6370243d1d5e46d1fda24e2c6
Connor Blakey <connorblakey96@gmail.com>
2016-10-27 00:39:15 +0100

    Added some buttons.

--- MonogameMapEditor/MainWindow.xaml.cs
+++ MonogameMapEditor/MainWindow.xaml.cs
@@ -23,6 +23,46 @@ namespace MonogameMapEditor
         public MainWindow()
         {
             InitializeComponent();
+            //DisplayGrid();
+        }
+
+        private void DisplayGrid()
+        {
+            int size = 15;
+            int top = 0;
+            int left = 0;
+
+            for (int row = 0; row < 10; row++)
+            {
+                for (int col = 0; col < 10; col++)
+                {
+                    Rectangle rect = new Rectangle();
+                    rect.Width = size; rect.Height = size;
+                    rect.Fill = Brushes.Blue;
+
+                    Canvas.SetTop(rect, top);
+                    Canvas.SetLeft(rect, left);
+                    map.Children.Add(rect);
+
+                    left += (size + 1);
+                }
+
+                left = 0;
+                top += (size + 1);
+            }
+        }
+
+        private void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Point pt = e.GetPosition((UIElement)sender);
+            HitTestResult result = VisualTreeHelper.HitTest(map, pt);
+
+            if (result != null)
+            {
+                // Perform action on hit visual object.
+                Rectangle rect = (Rectangle)result.VisualHit;
+                rect.Fill = Brushes.Green;
+            }
         }
     }
 }
